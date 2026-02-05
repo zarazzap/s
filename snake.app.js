4e5b0df2889800242b0cbe17c721b0b9b74af9ee
Lina Z <zarazzap@eduroam-int-dhcp-97-72-27.ucl.ac.uk>
2026-02-05 18:22:43 +0000

    Slow snake tick slightly

--- snake.app.js
+++ snake.app.js
@@ -21,7 +21,7 @@ const chooseDenBtn = document.querySelector('#choose-den');
 const GRID_SIZE = 14;
 let CELL = 32;
 const ASSET_VERSION = '2026-02-05-5';
-const TICK_MS = 120;
+const TICK_MS = 150;
 
 function resizeCanvas() {
   const parentWidth = canvas.parentElement ? canvas.parentElement.clientWidth : GRID_SIZE * CELL;
